943c0c78e7e74b7be495356b642f57fcb7d1108e
martin ponce espinoza <martin.ponce@techsoft.com.mx>
2024-02-23 18:04:41 -0700

    Agregar archivos de proyecto.

---
+++ Act6/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace Act6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            fechaNacimiento.MaximumDate = DateTime.Now.AddMonths(-1);
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var años = 0;
+            var meses = 0;
+            var fecha = fechaNacimiento.Date;
+            while (fecha < fechaActual.Date)
+            {
+                fecha = fecha.AddMonths(1);
+                meses++;
+                if (meses == 12)
+                {
+                    años++;
+                    meses = 0;
+                }
+            }
+            if((años > 0) || (meses > 0) )
+            {
+                label.Text = $"Edad: {años} años y {meses} meses";
+            }
+            else
+            {
+                await DisplayAlert("Aviso", "La fecha de nacimiento no puede ser mayor a la fecha actual", "Aceptar");
+            }
+        }
+    }
+
+}
